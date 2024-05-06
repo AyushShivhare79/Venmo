94374e6623da30ceeda6eb731de0e47acdaf486b
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-05-06 15:27:58 +0530

    removed duplicate folder

--- Venmo-main/apps/user/app/Test.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useBalance } from "@repo/store/useBalance";
-
-export const Test = () => {
-  const balance = useBalance();
-  return <>Hii there my balane is {balance}</>;
-};
