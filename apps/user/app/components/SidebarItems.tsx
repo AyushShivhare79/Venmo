e7fb06edf7e2f4785cf39fe7393e32b43ff896dd
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-05-07 22:44:49 +0530

    created dashboard and layout

---
+++ apps/user/app/components/SidebarItems.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+interface sidebarComp {
+  title: string;
+  href: string;
+  icon: any;
+}
+
+export function SidebarItems({ title, href, icon }: sidebarComp) {
+  const router = useRouter();
+  const params = usePathname();
+  const select = href === params;
+  return (
+    <>
+      <div
+        onClick={() => {
+          router.push(href);
+        }}
+        className={`flex ${select ? "text-purple-500" : "text-gray-500"} font-bold pl-8 pt-3 cursor-pointer`}
+      >
+        <div className="flex gap-1">
+          <div>{icon}</div>
+          <div>{title}</div>
+        </div>
+      </div>
+    </>
+  );
+}
